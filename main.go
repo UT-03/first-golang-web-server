4a9881f64a1a715ae477f98fd91d263d64d89fac
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-23 16:20:42 +0530

    feat: add route to post an album

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -14,7 +13,6 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
@@ -24,11 +22,22 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
